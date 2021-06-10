38f5672479a075a6345241ac48b2d5a3c99c714b
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-06-10 12:35:41 +0530

    Implemented core related interfaces & classes

---
+++ MicroRabbit.Core/Bus/IEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ MicroRabbit.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
